2efefdd6a868f4f04ff38a782dc435dc625a442f
Nevacat <nevacattery217@naver.com>
2023-04-06 01:35:08 +0900

    :package: 파일구조 변경 및 스타일 test

---
+++ src/components/NewTask/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Test } from './style'
+
+function index() {
+  return (
+    <Test>index</Test>
+  )
+}
+
+export default index
---
+++ src/components/common/Layout/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as S from './style';
+
+function Layout() {
+  return <S.Layout>Layout</S.Layout>
+  
+};
+
+export default Layout
