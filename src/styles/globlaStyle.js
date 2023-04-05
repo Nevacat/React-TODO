97bc8688dac10879e554574299c65cf5cd4e1858
Nevacat <nevacattery217@naver.com>
2023-04-06 01:32:22 +0900

    :lipstick: globalStyle 생성

---
+++ src/styles/globlaStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyled = createGlobalStyle`
+  ${reset}
+  :root{
+    font-size: 10px;
+    font-weight: 400;
+    font-family: 'Noto Sans', sans-serif; 
+  }
+  body{
+    width: 100vw;
+    
+}
+`
