2311215b7d204a2b71554e215f851cdb8555cec6
Nevacat <nevacattery217@naver.com>
2023-04-05 23:02:30 +0900

    :lipstick: theme 공통 스타일 작성
    font, color, backgroun 등 작성완료(추가 작성예정)

---
+++ src/styles/theme.js
@@ -0,0 +1,37 @@
+const fontSize = {
+  small: '1.2rem',
+  regular: '1.6rem',
+  semiLarge: '2rem',
+  Large: '2.4rem',
+  Big: '6.4rem',
+};
+
+const fontWeight = {
+  nomal: '400',
+  bold: '700',
+  heavy: '900',
+};
+
+const darkVersion = {
+  background: '#141414',
+  fontPrimary: '#ffffff',
+  box:'#292A2D'
+};
+
+const color = {
+  pink: '#DB7CB1',
+  blue: '#78ACD5',
+  purple: '#919FEB',
+  red: '#FF2460',
+  black: '#141414',
+  white: '#ffffff',
+};
+
+const theme = {
+  fontSize,
+  fontWeight,
+  darkVersion,
+  color,
+};
+
+export default theme;
