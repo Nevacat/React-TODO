0922a4384704d4a76c17ddea803bd49bfff7d869
Nevacat <nevacattery217@naver.com>
2023-04-07 03:39:35 +0900

    :sparkles: Conponent 연결 및 Layout 디자인 구성

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import * as S from './style';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
 
 function Layout() {
-  return <S.Layout>Layout</S.Layout>
-  
-};
+  return (
+    <S.Layout>
+      <Header/>
+      <Outlet />
+    </S.Layout>
+  );
+}
 
-export default Layout
+export default Layout;
--- src/components/common/Layout/style.js
+++ src/components/common/Layout/style.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const Layout = styled.div`
-  background-color: blue;
+  background-color: ${({theme})=>theme.darkVersion.background};
+  width: 39rem;
+  height: 80.4rem;
+  padding: 1rem 1.5rem 5rem;
+  border-radius: 1.2rem;
+  position: relative;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  color: ${({theme})=>theme.darkVersion.fontPrimary};
+  overflow:hidden;
 `
