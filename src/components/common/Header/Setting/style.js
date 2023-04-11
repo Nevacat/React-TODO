376ba2e319513562be81a412edf8bd3db29481da
Nevacat <nevacattery217@naver.com>
2023-04-12 01:22:09 +0900

    Close: setting 기능은 잠시 클로징 되었습니다.

--- src/components/common/Header/Setting/style.js
+++ src/components/common/Header/Setting/style.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const Setting = styled.i`
-  cursor: pointer;
+  /* cursor: pointer; */
   width: 2.4rem;
   height: 2.4rem;
+  opacity: 0;
   ${(props) => (
     props.hide && props.theme.hide
   )}
