e623c57d073c411ccdcddc8d7ff5329d54c2f409
Nevacat <nevacattery217@naver.com>
2023-04-12 01:08:49 +0900

    feat: QueryClient 전역관리

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,24 @@ import Header from './components/common/Header';
 import theme from './styles/Theme';
 import Router from './routes/routes';
 import { GlobalStyled } from './styles/globlaStyle';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    },
+  },
+})
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyled />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyled />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
