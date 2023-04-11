425c01d7d5e8cc58826101e01c3696e7a1a76088
Nevacat <nevacattery217@naver.com>
2023-04-12 01:14:32 +0900

    feat: BackBtn Component 생성
    주소에 따른 뒤로가기 기능 구현

--- src/components/common/Header/BackBtn/index.jsx
+++ src/components/common/Header/BackBtn/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { AiFillLeftSquare } from "react-icons/ai";
 import * as S from './style'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate} from 'react-router-dom';
 function Back() {
   const location = useLocation()
+  const navigate = useNavigate()
+
+  const handleBack = ()=>{
+    location.pathname==='/note'
+    ?navigate('/')
+    :navigate('/note')
+  }
+
   return (
-    <S.Back hide={location.pathname==='/'}>
+    <S.Back hide={location.pathname==='/'} onClick={handleBack}>
       <AiFillLeftSquare/>
     </S.Back>
   )
--- src/components/common/Header/BackBtn/style.js
+++ src/components/common/Header/BackBtn/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Back = styled.i`
+  cursor: pointer;
   width:2.4rem;
   height:2.4rem;
   ${(props) => (
